ba541c75433d7357ca065440b5a0c99f15901484
Tristen Briggz <skaterbriggz@gmail.com>
2019-11-09 16:23:54 -0500

    implemented overrides

---
+++ Assets/_Scripts/GameController.cs
@@ -0,0 +1,187 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using System.IO;
+using System.Linq;
+
+public class GameController : MonoBehaviour
+{
+    [Header("Scene Game Objects")]
+    public GameObject cloud;
+    public GameObject island;
+    public int numberOfClouds;
+    public List<GameObject> clouds;
+
+    [Header("Audio Sources")]
+    public SoundClip activeSoundClip;
+    public AudioSource[] audioSources;
+
+    [Header("Scoreboard")]
+    [SerializeField]
+    private int _lives;
+
+    [SerializeField]
+    private int _score;
+
+    public Text livesLabel;
+    public Text scoreLabel;
+    public Text highScoreLabel;
+
+
+    //public HighScoreSO highScoreSO;
+
+    [Header("UI Control")]
+    public GameObject startLabel;
+    public GameObject startButton;
+    public GameObject endLabel;
+    public GameObject restartButton;
+
+    [Header("Game Settings")]
+    public ScoreBoard scoreBoard;
+
+    [Header("Scene Settings")]
+    public SceneSettings activeSceneSettings;
+    public List<SceneSettings> sceneSettings;
+
+
+    // public properties
+    public int Lives
+    {
+        get
+        {
+            return _lives;
+        }
+
+        set
+        {
+            _lives = value;
+            scoreBoard.lives = _lives;
+
+            if(_lives < 1)
+            {
+                
+                SceneManager.LoadScene("End");
+            }
+            else
+            {
+                livesLabel.text = "Lives: " + _lives.ToString();
+            }
+           
+        }
+    }
+
+    public int Score
+    {
+        get
+        {
+            return _score;
+        }
+
+        set
+        {
+            _score = value;
+            scoreBoard.score = _score;
+
+            if (scoreBoard.highScore < _score)
+            {
+                scoreBoard.highScore = _score;
+            }
+            scoreLabel.text = "Score: " + _score;
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameObjectInitialization();
+        SceneConfiguration();
+    }
+
+    private void GameObjectInitialization()
+    {
+
+        startLabel = GameObject.Find("StartLabel");
+        endLabel = GameObject.Find("EndLabel");
+        startButton = GameObject.Find("StartButton");
+        restartButton = GameObject.Find("RestartButton");
+
+        //scoreBoard = Resources.FindObjectsOfTypeAll<ScoreBoard>()[0] as ScoreBoard;
+    }
+
+
+    private void SceneConfiguration()
+    {
+        // selects the current scene
+        Scene sceneToCompare = (Scene)Enum.Parse(typeof(Scene), SceneManager.GetActiveScene().name.ToUpper());
+
+        //Compares the settings list with the current scene
+        var query = from settings in sceneSettings
+                    where settings.scene == sceneToCompare
+                    select settings;
+
+        //Sets the appropriate settings for the loaded scene
+        activeSceneSettings = query.ToList().First();
+
+
+        {
+            //Checks if the main scene settings are active and sets up initial lives and score
+            if(activeSceneSettings.scene == Scene.START)
+            {
+                Lives = 5;
+                Score = 0;
+            }
+
+            // applies all scene settings from the Scriptabe object
+            activeSoundClip = activeSceneSettings.activeSoundClip;
+            scoreLabel.enabled = activeSceneSettings.scoreLabelEnabled;
+            livesLabel.enabled = activeSceneSettings.livesLabelEnabled;
+            highScoreLabel.enabled = activeSceneSettings.highScoreLabelEnabled;
+            startLabel.SetActive(activeSceneSettings.startLabelActive);
+            endLabel.SetActive(activeSceneSettings.endLabelActive);
+            startButton.SetActive(activeSceneSettings.startButtonActive);
+            restartButton.SetActive(activeSceneSettings.restartButtonActive);
+
+            // Assigns text values to labels from the scoreboard Scriptable object
+            livesLabel.text = "Lives: " + scoreBoard.lives;
+            scoreLabel.text = "Score: " + scoreBoard.score;
+            highScoreLabel.text = "High Score:" + scoreBoard.highScore;
+        }
+
+
+
+        if ((activeSoundClip != SoundClip.NONE) && (activeSoundClip != SoundClip.NUM_OF_CLIPS))
+        {
+            AudioSource activeAudioSource = audioSources[(int)activeSoundClip];
+            activeAudioSource.playOnAwake = true;
+            activeAudioSource.loop = true;
+            activeAudioSource.volume = 0.5f;
+            activeAudioSource.Play();
+        }
+
+
+
+        // creates an empty container (list) of type GameObject
+        clouds = new List<GameObject>();
+
+        for (int cloudNum = 0; cloudNum < numberOfClouds; cloudNum++)
+        {
+            clouds.Add(Instantiate(cloud));
+        }
+
+        Instantiate(island);
+    }
+
+    // Event Handlers
+    public void OnStartButtonClick()
+    {
+        SceneManager.LoadScene("Main");
+    }
+
+    public void OnRestartButtonClick()
+    {
+        SceneManager.LoadScene("Main");
+    }
+}
---
+++ Assets/_Scripts/Scene Settings/SceneSettings.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[CreateAssetMenu(fileName = "SceneSettings", menuName = "Scene/Settings")]
+[System.Serializable]
+public class SceneSettings : ScriptableObject
+{
+    [Header("Scene Configuration")]
+    public Scene scene;
+    public SoundClip activeSoundClip;
+    
+    [Header("Scoreboard Labels")]
+    public bool scoreLabelEnabled;
+    public bool livesLabelEnabled;
+    public bool highScoreLabelEnabled = false;
+
+
+    [Header("Scene Labels")]
+    public bool startLabelActive;
+    public bool endLabelActive;
+
+    [Header("Scene Buttons")]
+    public bool startButtonActive;
+    public bool restartButtonActive;
+}
